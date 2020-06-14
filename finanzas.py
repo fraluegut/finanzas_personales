822b2d8b324c1348ae06826cd815003b263d5d78
fraluegut <franciscojavierluengo@outlook.com>
2020-06-14 13:41:36 +0200

    Algunas mejoras en la conexión.

--- finanzas.py
+++ finanzas.py
@@ -13,76 +13,27 @@ import xlrd
 # 3. Por cada fila de archivo excel: comprobar si ese registro existe y si no, subirlo a la base de datos.
 
 import pandas as pd
-from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
 
-## DATABASE LOCALHOST
-class DataBase:
-    def __init__(self):
-        self.connection = pymysql.connect(
-            host='localhost', # ip
-            user='root',
-            password='',
-            db='finanzas'
-        )
+import mysql.connector
 
-        self.cursor = self.connection.cursor()
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="finanzas"
+)
 
-        print("Conexión establecida exitosamente")
+mycursor = mydb.cursor()
 
+sql = "INSERT INTO finanzas_personales(fecha_valor, fecha_operacion, importe, saldo) VALUES(%s,%s,%s,%s)"
+val = ("2020-06-02", "2020-06-02", "1", "1")
+mycursor.execute(sql, val)
 
+mydb.commit()
 
-archivo_banco = 'export2020612.xlsx'
-df = pd.read_excel(archivo_banco)
-print(df)
+print(mycursor.rowcount, "record inserted.")
 
-index = df.index
-number_of_rows = len(index)
-
-print(number_of_rows)
-
-
-# Get list of columns
-columnas = list(df.columns.values)
-print(columnas)
-
-
-for index, row in df.iterrows():
-    # if row['Unnamed: 0'] > 7:
-    if index > 6:
-        print(row['Unnamed: 0'], row['Unnamed: 1'], row['CUENTA 123 SMART.'], row['FECHA'], row['Unnamed: 4'])
-        database = DataBase()
-        cursor = self.connection.cursor()
-        self.cursor.execute(sql)
-    else:
-        print(index)
-        print("Valor no incluido")
-
-
-def insert_books(books):
-    query = "INSERT INTO finanzas_personales(id, fecha_valor, fecha_operacion, importe, saldo) " \
-            "VALUES(%s,%s,%s,%s,%s)"
-
-    try:
-        db_config = read_db_config()
-        conn = MySQLConnection(**db_config)
-
-        cursor = conn.cursor()
-        cursor.executemany(query, books)
-
-        conn.commit()
-    except Error as e:
-        print('Error:', e)
-
-    finally:
-        cursor.close()
-        conn.close()
-
-# for row in df:
-#     if df[0] > 7:
-#         print(df[0], df[1])
-#     else:
-#         print("No")
+#
 # ## DATABASE LOCALHOST
 # class DataBase:
 #     def __init__(self):
@@ -97,9 +48,26 @@ def insert_books(books):
 #
 #         print("Conexión establecida exitosamente")
 #
-#     def select_user(self):
 #
-#         sql = 'SELECT * FROM users'
+#
+# archivo_banco = 'export2020612.xlsx'
+# df = pd.read_excel(archivo_banco)
+# print(df)
+#
+# index = df.index
+# number_of_rows = len(index)
+#
+# print(number_of_rows)
+#
+#
+# # Get list of columns
+# columnas = list(df.columns.values)
+# print(columnas)
+#
+# def insert_datos(self):
+#
+#         sql = "INSERT INTO finanzas_personales(id, fecha_valor, fecha_operacion, importe, saldo) " \
+#             "VALUES(%s,%s,%s,%s,%s)"
 #         try:
 #             self.cursor.execute(sql)
 #             user = self.cursor.fetchone()
@@ -111,34 +79,99 @@ def insert_books(books):
 #             raise
 #
 # database = DataBase()
-# database.select_user()
-
-
-### DASH
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# insert_datos()
 #
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# for index, row in df.iterrows():
+#     # if row['Unnamed: 0'] > 7:
+#     if index > 6:
+#         print(row['Unnamed: 0'], row['Unnamed: 1'], row['CUENTA 123 SMART.'], row['FECHA'], row['Unnamed: 4'])
+#         database = DataBase()
+#         cursor = self.connection.cursor()
+#         self.cursor.execute(sql)
+#     else:
+#         print(index)
+#         print("Valor no incluido")
 #
-# app.layout = html.Div(children=[
-#     html.H1(children='Finanzas personales'),
 #
-#     html.Div(children='''
-#         Visor de economía intrafamiliar
-#     '''),
+# def insert_books(books):
+#     query = "INSERT INTO finanzas_personales(id, fecha_valor, fecha_operacion, importe, saldo) " \
+#             "VALUES(%s,%s,%s,%s,%s)"
 #
-#     dcc.Graph(
-#         id='example-graph',
-#         figure={
-#             'data': [
-#                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-#                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-#             ],
-#             'layout': {
-#                 'title': 'Dash Data Visualization'
-#             }
-#         }
-#     )
-# ])
+#     try:
+#         db_config = read_db_config()
+#         conn = MySQLConnection(**db_config)
 #
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+#         cursor = conn.cursor()
+#         cursor.executemany(query, books)
+#
+#         conn.commit()
+#
+#
+#     finally:
+#         cursor.close()
+#         conn.close()
+#
+# # for row in df:
+# #     if df[0] > 7:
+# #         print(df[0], df[1])
+# #     else:
+# #         print("No")
+# # ## DATABASE LOCALHOST
+# # class DataBase:
+# #     def __init__(self):
+# #         self.connection = pymysql.connect(
+# #             host='localhost', # ip
+# #             user='root',
+# #             password='',
+# #             db='finanzas'
+# #         )
+# #
+# #         self.cursor = self.connection.cursor()
+# #
+# #         print("Conexión establecida exitosamente")
+# #
+# #     def select_user(self):
+# #
+# #         sql = 'SELECT * FROM users'
+# #         try:
+# #             self.cursor.execute(sql)
+# #             user = self.cursor.fetchone()
+# #
+# #             print("Id:", user[0])
+# #             print("Username:", user[1])
+# #             print("Email:", user[2])
+# #         except Exception as e:
+# #             raise
+# #
+# # database = DataBase()
+# # database.select_user()
+#
+#
+# ### DASH
+# # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# #
+# # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# #
+# # app.layout = html.Div(children=[
+# #     html.H1(children='Finanzas personales'),
+# #
+# #     html.Div(children='''
+# #         Visor de economía intrafamiliar
+# #     '''),
+# #
+# #     dcc.Graph(
+# #         id='example-graph',
+# #         figure={
+# #             'data': [
+# #                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+# #                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+# #             ],
+# #             'layout': {
+# #                 'title': 'Dash Data Visualization'
+# #             }
+# #         }
+# #     )
+# # ])
+# #
+# # if __name__ == '__main__':
+# #     app.run_server(debug=True)
